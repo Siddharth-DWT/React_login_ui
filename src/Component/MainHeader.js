481c21679d906013a8a92496d5d7b4c81739cc94
Siddharth <smishra9120@gmail.com>
2021-10-28 23:27:22 +0530

    Fixed login button

--- src/Component/MainHeader.js
+++ src/Component/MainHeader.js
@@ -1,23 +1,11 @@
 import React  from 'react';
 import {Navbar, Nav, Button} from 'react-bootstrap';
 import {
-    Link, useHistory
+    Link
 } from "react-router-dom";
-import Cookies from 'js-cookie';
-import { toast } from 'react-toastify';
+
 
 export default function Header() {
-    const history = useHistory();
-    Cookies.remove('name')
-
-    const logout = async () => {
-        Cookies.remove("user-info")
-        sessionStorage.removeItem('user_sid');
-        localStorage.clear();
-        toast.success("Logout Successfully ")
-        history.push("/")
-    }
-
     return (
         <div>
             <Navbar bg="dark" variant="dark">
@@ -26,15 +14,15 @@ export default function Header() {
                         <Navbar.Brand>LOGO</Navbar.Brand>
                     </div>
                 <Nav className="nav_bar_wrapper">
-                    <Nav><Link to="/list" className="userheader">Home</Link></Nav>
-                    <Nav><Link to="/create" className="userheader">About Us</Link></Nav>
-                    <Nav><span onClick={logout} className="userheader">Contact</span></Nav>
+                    <Nav><Link  className="userheader">Home</Link></Nav>
+                    <Nav><Link  className="userheader">About Us</Link></Nav>
+                    <Nav><span  className="userheader">Contact</span></Nav>
 
                 </Nav>
                 <div className="header_btn">
                     <Button variant="outline-primary" size="lg" className="login-register-btn login_header">
                         <Link to="/login">LOGIN</Link>
-                    </Button>{' '}
+                    </Button>
                     <Button variant="outline-primary" size="lg" className="login-register-btn register_header">
                         <Link to="/Register">Register</Link>
                     </Button>
